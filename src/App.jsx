5df0f18626181edbff6c55653847237982aed2b5
kaexh <ebaneckkeziah@gmail.com>
2025-08-30 15:03:45 +0100

    AgriTrade Frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React  from "react"
+import About from '../src/components/pages/about/about';
+
+
+function App() {
+  
+  return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+  )
+}
+
+export default App
